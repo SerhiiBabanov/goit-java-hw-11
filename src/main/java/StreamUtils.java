37cf3584aa191d0403bbdda3f26835b79c907028
serhiibabanov <serhii.babanov@gmail.com>
2022-05-24 21:42:21 -0400

    add method for task5

--- src/main/java/StreamUtils.java
+++ src/main/java/StreamUtils.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
@@ -35,5 +36,15 @@ public class StreamUtils {
     //task 4
     public Stream<Long> getStreamRandomLong(Long seed, Long a, Long c, Long m) {
         return Stream.iterate(new Long[]{seed, a, c, m}, n -> new Long[]{(n[1] * n[0] + n[2]) % n[3], n[1], n[2], n[3]}).map(n -> n[0]);
+    }
+    //task 5
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second){
+        Iterator<T> iteratorFirst = first.iterator();
+        Iterator<T> iteratorSecond = second.iterator();
+        Stream<T> resultStream = Stream.empty();
+        while (iteratorFirst.hasNext() && iteratorSecond.hasNext()){
+            resultStream = Stream.concat(resultStream, Stream.of(iteratorFirst.next(), iteratorSecond.next()));
+        }
+        return resultStream;
     }
 }
